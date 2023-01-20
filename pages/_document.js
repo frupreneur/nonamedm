dc883d09ab9810bd429e9cd120b5f0910e0764c4
frupreneur <frupreneur@gmail.com>
2023-01-21 00:03:15 +0100

    updated theme color in browser

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
-        <meta name="theme-color" content="#fff" />
+        <meta name="theme-color" content="#f51cfd" />
       </Head>
       <body>
         <Main />
